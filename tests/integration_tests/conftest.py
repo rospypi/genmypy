fec7afc64db6867ae0e1d837d218371902739dba
Yuki Igarashi <me@bonprosoft.com>
2021-04-27 20:55:50 +0900

    Add module finder to control module listing (#13)
    * Add module finder to control module listing
    
    * Add tests

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -12,15 +12,33 @@ def ros_share_directory():
 
 
 @pytest.fixture
+def ros_python_library_directory():
+    # type: () -> str
+    return "/opt/ros/melodic/lib/python2.7/dist-packages/"
+
+
+@pytest.fixture
 def std_msgs_path(ros_share_directory):
     # type: (str) -> str
     return os.path.join(ros_share_directory, "std_msgs")
 
 
 @pytest.fixture
+def std_msgs_py_path(ros_python_library_directory):
+    # type: (str) -> str
+    return os.path.join(ros_python_library_directory, "std_msgs")
+
+
+@pytest.fixture
 def sensor_msgs_path(ros_share_directory):
     # type: (str) -> str
     return os.path.join(ros_share_directory, "sensor_msgs")
+
+
+@pytest.fixture
+def sensor_msgs_py_path(ros_python_library_directory):
+    # type: (str) -> str
+    return os.path.join(ros_python_library_directory, "sensor_msgs")
 
 
 @pytest.fixture
--- tests/integration_tests/test_modules.py
+++ tests/integration_tests/test_modules.py
@@ -7,25 +7,39 @@ from genpy_stubgen.cli import run_module_stubgen
 from .utils import assert_output_equals, message_path, service_path
 
 
-def test_std_msgs_msg(expected_dir, std_msgs_path):
-    # type: (str, str) -> None
+def test_std_msgs_msg(expected_dir, std_msgs_path, std_msgs_py_path):
+    # type: (str, str, str) -> None
     package = "std_msgs"  # type: str
     package_dir = message_path(std_msgs_path)
+    package_py_dir = message_path(std_msgs_py_path)
     expected_dir = os.path.join(expected_dir, package, "msg")
 
     with tempfile.TemporaryDirectory() as td:
-        run_module_stubgen(package_dir, td)
+        run_module_stubgen(package_dir, td, "genmsg")
+
+        assert_output_equals(expected_dir, td, "__init__.pyi")
+
+    with tempfile.TemporaryDirectory() as td:
+        run_module_stubgen(package_py_dir, td, "py")
 
         assert_output_equals(expected_dir, td, "__init__.pyi")
 
 
-def test_sensor_msgs_srv(expected_dir, std_msgs_path, sensor_msgs_path):
-    # type: (str, str, str) -> None
+def test_sensor_msgs_srv(
+    expected_dir, std_msgs_path, sensor_msgs_path, sensor_msgs_py_path
+):
+    # type: (str, str, str, str) -> None
     package = "sensor_msgs"  # type: str
     package_dir = service_path(sensor_msgs_path)
+    package_py_dir = service_path(sensor_msgs_py_path)
     expected_dir = os.path.join(expected_dir, package, "srv")
 
     with tempfile.TemporaryDirectory() as td:
-        run_module_stubgen(package_dir, td)
+        run_module_stubgen(package_dir, td, "genmsg")
+
+        assert_output_equals(expected_dir, td, "__init__.pyi")
+
+    with tempfile.TemporaryDirectory() as td:
+        run_module_stubgen(package_py_dir, td, "py")
 
         assert_output_equals(expected_dir, td, "__init__.pyi")
