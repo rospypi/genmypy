a5cb8c8e17b3451de7e81d633a7a5f82e0c36b4e
Yuki Igarashi <me@bonprosoft.com>
2021-05-01 05:32:39 +0900

    Bump version to 0.2.0 (#33)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="genpyi",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(exclude=["tests"]),
     description="A Python stub generator from genmsg specs",
     long_description=open("README.rst").read(),
